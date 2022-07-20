0555750c9630f59ce771b8ad6c832dd602e4f0a5
Koh Jia Xian <kohjx96@gmail.com>
2022-07-20 21:57:06 +0800

    Setup Heroku requirements

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   app.use(cors());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
